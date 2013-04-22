56082b6fe57b83cd57ae18032374b86a84eb4a1c
Kevin ARBOUIN <lesmyrmidons@lesmyrmidons-EasyNote-MX36.(none)>
2013-04-22 23:21:12 +0200

    add install zsh

---
+++ install-zsh.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+# Install zsh
+sudo aptitude install zsh
+
+# Change shell for zsh
+chsh /bin/zsh
+
+# Install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Download zshrc
+wget https://github.com/lesmyrmidons/dotfiles/.zshrc
+
+# Move zshrc for current user
+mv .zshrc ~/
+
