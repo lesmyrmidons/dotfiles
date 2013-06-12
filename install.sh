0d29ee854d08b2aa44d2611fc8b36705e0433e10
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-06-13 00:12:54 +0200

    add install dotfiles

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CURRENT=`pwd`
+
+PACKAGE = 'git git-core curl terminator'
+
+sudo apt-get install $PACKAGE
+
+# Install composer.phar
+curl -s https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
+
+if [ -f ~/.gitconfig ] ; then
+    cat ~/.gitconfig > ~/gitconfig.backup
+    rm -f ~/.gitconfig
+    echo "Existing .gitconfig >>> gitconfig.backup"
+fi
+
+if [ -f ~/.gitignore_global ] ; then
+    cat ~/.gitignore_global > ~/gitignore_global.backup
+    rm -f ~/.gitignore_global
+    echo "Existing .gitignore_global >>> gitignore_global.backup"
+fi
+
+if [ -f ~/.config/terminator/config ] ; then
+    cat ~/.config/terminator/config > ~/.config/terminator/config.backup
+    rm -rf ~/.config/terminator
+    echo "Existing .config terminator >>> ~/.config/terminator/config.backup"
+fi
+
+ln -s $CURRENT/gitconfig ~/.gitconfig
+ln -s $CURRENT/gitignore_global ~/.gitignore_global
+ln -s $CURRENT/_config/terminator ~/.config/terminator
