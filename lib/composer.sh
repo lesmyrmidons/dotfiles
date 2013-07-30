3e20ec8416176210ca40e5be7830aa58e17fafce
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-07-30 18:19:05 +0200

    add comment for install composer

--- lib/composer.sh
+++ lib/composer.sh
@@ -9,4 +9,5 @@ echo "Install to composer.phar"
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin/
 
 # Rename composer.phar composer
+echo "Rename composer.phar composer"
 sudo mv /usr/local/bin/composer.phar /usr/local/bin/composer
